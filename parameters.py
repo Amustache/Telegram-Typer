e7ae73396a50de7b910beb8748e31372e3665a1b
Stache <hugo.hueber@epfl.ch>
2022-01-31 17:13:46 +0100

    Change timer to 2s

--- parameters.py
+++ parameters.py
@@ -1,6 +1,6 @@
 DB_PATH = "./db/database.db"
 RESALE_PERCENTAGE = 0.55
-TIME_INTERVAL = 1
+TIME_INTERVAL = 2
 FACTOR = 1.014_159_265_359
 # Quantities are stored as STRING, and last two digits are floating.
 CAP = int(1e300) * 100 + 99
